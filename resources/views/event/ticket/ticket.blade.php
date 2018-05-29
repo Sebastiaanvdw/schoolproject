d15b252fda387a9d8f30aaba6012c897eed24fe9
SebastisaanVDW <asr.vanderweel@gmail.com>
2018-05-29 09:56:41 +0200

    test commit

---
+++ resources/views/event/ticket/ticket.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <table style="border: solid">
+        <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Location</th>
+            <th>Begin time</th>
+            <th>End time</th>
+            <th>Age restriction</th>
+        </tr>
+        @foreach($events as $event)
+            <tr>
+                <td>{{ $event->name }}</td>
+                <td>{{ $event->location }}</td>
+                <td>{{ $event->date }}</td>
+                <td>{{ $event->starttime }}</td>
+                <td>{{ $event->endtime }}</td>
+                <td>{{ $event->agerestriction }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
