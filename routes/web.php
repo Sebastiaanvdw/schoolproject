7b828658ef5662d89055d7496d3a037b3faeaaa3
Your Name <casperbosma@gmail.com>
2018-06-14 18:51:03 +0200

    controller+search

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'posts'], function() {
+    Route::get('/', 'PostController@index');
+    Route::match(['get', 'post'], 'create', 'PostController@create');
+    Route::match(['get', 'put'], 'update/{id}', 'PostController@update');
+    Route::get('show/{id}', 'PostController@show');
+    Route::delete('delete/{id}', 'PostController@destroy');
+});
