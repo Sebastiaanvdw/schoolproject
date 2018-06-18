8e800f4193dd4be7a7e6515f4e7de7d83b49caeb
Caperb <39518636+Caperb@users.noreply.github.com>
2018-06-18 23:17:00 +0200

    minor fixes

--- resources/views/merchandises/ajax.blade.php
+++ resources/views/merchandises/ajax.blade.php
@@ -1,27 +1,5 @@
 @extends('merchandises.master')
 
-@section('css')
-    <style>
-        .loading {
-            background: lightgrey;
-            padding: 15px;
-            position: fixed;
-            border-radius: 4px;
-            left: 50%;
-            top: 50%;
-            text-align: center;
-            margin: -40px 0 0 -50px;
-            z-index: 2000;
-            display: none;
-        }
-
-        .form-group.required label:after {
-            content: " *";
-            color: red;
-            font-weight: bold;
-        }
-    </style>
-@endsection
 @section('content')
     <div id="content">
         @include('merchandises.index')
