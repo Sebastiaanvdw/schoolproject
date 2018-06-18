2e4e3edaa962fdcbb2788ed6a549a92b610f23cd
Tim Minderman <Timminderman@hotmail.com>
2018-06-18 21:15:57 +0200

    flexbox toegevoegd aan show.blade

--- resources/views/vacancies/show.blade.php
+++ resources/views/vacancies/show.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
     <div class="show_container">
-    <table>
-        <tr class="table_header">
-            <th>Title:</th>
-            <th>Occupation:</th>
-            <th>Description:</th>
-        </tr>
-            <tr class="table_info">
-                <td>{{$vacancy->title}}</td>
-
-                <td>{{$vacancy->occupation->occupationName}}</td>
-
-                <td>{{$vacancy->description}}</td>
-            </tr>
-    </table>
+    <div class="show_header_row">
+        <h1 class="show_header">Vacancy</h1>
     </div>
+        <form>
+            <ul class="flex-outer">
+                <li>
+                    <label for="title">Title:</label>
+                    <p>{{$vacancy->title}}</p>
+                </li>
+                <li>
+                    <label for="occupation">Occupation:</label>
+                    <p>{{$vacancy->occupation->occupationName}}</p>
+                </li>
+                <li>
+                    <label for="description">Description:</label>
+                    <p>{{$vacancy->description}}</p>
+                </li>
+            </ul>
+        </form>
     </div>
     <div class="back">
     <a href="{{ url('vacancies') }}" class="back_button">Back</a>
